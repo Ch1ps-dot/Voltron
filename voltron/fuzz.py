ff0d658e54f3f79eeec996a658ac4b5590486e52
Ch1ps-dot <2241180437@qq.com>
2025-11-07 15:37:19 +0800

    up: framewrok

--- voltron/fuzz.py
+++ voltron/fuzz.py
@@ -1,9 +1,16 @@
-from .llm import utils
+from .llm import LLM
+from .nio import Nio
+from .executor import Executor
+import subprocess
 
-def initialize_llm():
-    clt = utils.login()
+class Fuzzer:
+    def __init__(self, stype:str, host:str, port:int, sut_path:str,script_path:str = '') -> None:
+        self.llm = LLM()
+        self.executor = Executor(script_path, sut_path)
+        self.nio = Nio(stype, host, port)
 
-def fuzz_one():
-    clt = utils.login()
-    ans = utils.chatllm(clt, "Hello")
-    return ans
+    def fuzz_one(self):
+        pass
+    
+    def fuzz_loop(self):
+        pass
